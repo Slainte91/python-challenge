2201f8d62232db41c37eac8b52e8cdc45808f4a2
John Carroll <jslaincarroll@gmail.com>
2021-02-28 14:46:32 -0600

    new python script modifications

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,3 +3,27 @@ import csv
 
 csvpath = os.path.join('..', 'Resources', 'PyBank_budget_data.csv')
 
+monthCounter = 0
+greatestInc = 0
+greatestDec = 0
+averageProfit = 0
+netProfit = 0
+
+with open(csvpath, "r", encoding='utf8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csvheader = next(csvreader) # pop one row
+
+for row in csvreader:
+    monthCounter += 1
+    currentProfit = row[1]
+    netprofit += currentProfit
+
+    if currentProfit > greatestInc:
+        greatestInc = currentProfit
+
+    if currentProfit < greatestDec:
+        greatestDec = currentProfit
+
+    
+
+
