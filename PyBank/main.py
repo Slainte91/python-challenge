70a496c88e3d2443ec5865b68b79841de1881948
John Carroll <jslaincarroll@gmail.com>
2021-03-03 12:25:19 -0600

    finished commenting out code and including sources used

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,7 +2,7 @@ import os
 import csv
 
 csvpath = os.path.join("Resources", "budget_data.csv")
-
+#initialize variables
 monthCounter = 0
 greatestInc = 0
 greatestDec = 0
@@ -19,32 +19,43 @@ currentProfit = 0
 
 with open(csvpath, "r", encoding='utf8', newline ='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+    #skip header row
     csvheader = next(csvreader) 
 
     for row in csvreader:
+        #increment months by 1 each row
         monthCounter += 1
+        #figure out our months current profit to compare to last month's
         currentProfit = int(row[1])
+        #add to our total profit
         netProfit += currentProfit
+        #calculate change and add to our list of recorded changes
         changeProfit = currentProfit - lastProfit
         recordedChanges.append(changeProfit)
 
+        #set new variables if greater increase than previous
         if changeProfit > greatestInc:
             greatestInc = changeProfit
             greatestIncMonth = row[0]
 
+        #set new variables if greater decrease than previous
         if changeProfit < greatestDec:
             greatestDec = changeProfit
             greatestDecMonth = row[0]
 
+        #set current profit to last month to continue comparing
         lastProfit = currentProfit
-
+#first index is not an actual change since it's where we started so remove it
 recordedChanges.pop(0)
 
+#calculate total changes
 for change in recordedChanges:
     totalChanges += change
 
+#average our changes
 averageChangeProfit = round(totalChanges/(monthCounter-1),2) 
 
+#print analysis to terminal
 print("Financial Analysis")
 print("-------------------------")
 print(f"Total Months: {monthCounter}")
@@ -53,6 +64,7 @@ print(f"Average Change: ${averageChangeProfit}")
 print(f"Greatest Increase in Profits: {greatestIncMonth} (${greatestInc})")
 print(f"Greatest Decrease in Profits: {greatestDecMonth} (${greatestDec})")
 
+#print analysis to txt file in analysis folder
 output = os.path.join("analysis", "analysis.txt")
 with open(output, "w", newline = "", encoding = "utf8") as txtfile:
     # used https://stackoverflow.com/questions/36571560/directing-print-output-to-a-txt-file/36571602
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,6 +3,7 @@ import csv
 
 csvpath = os.path.join("Resources", "election_data.csv")
 
+#initialize our variables
 greatestVotes = 0
 greatestVotesName = ""
 percentVotes = 0
@@ -13,15 +14,20 @@ votes = []
 
 with open(csvpath, "r", encoding='utf8', newline ='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+    #skip the header
     csvheader = next(csvreader) 
 
     for row in csvreader:
+        #increment by one our total votes for each vote cast
         totalVotes += 1
+        #add the name of those votes to our list so we may count it later
         votes.append(row[2])
 
+        #get a list of each candidate's name once
         if row[2] not in candidates:
             candidates.append(row[2])
 
+#print our analysis to both the terminal and a text file
 output = os.path.join("analysis", "analysis.txt")
 with open(output, "w", newline = "", encoding = "utf8") as txtfile:
     # used https://stackoverflow.com/questions/36571560/directing-print-output-to-a-txt-file/36571602
@@ -35,9 +41,13 @@ with open(output, "w", newline = "", encoding = "utf8") as txtfile:
     print("-------------------------", file=txtfile)
 
     for candidate in candidates:
+        #count our votes using count method I found here https://www.w3schools.com/python/python_lists_methods.asp
         candidateVotes = votes.count(candidate)
+        #calculate percent of votes gotten and round to 3 decimal places using round which
+        #I found here https://www.w3schools.com/python/ref_func_round.asp
         percentVotes = round((candidateVotes/totalVotes)*100,3)
-
+        
+        #check if candidate has received more votes than previous ones to name the winner
         if candidateVotes > greatestVotes:
             greatestVotes = candidateVotes
             greatestVotesName = candidate
